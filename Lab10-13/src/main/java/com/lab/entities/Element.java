d6e2c6543fdc5d2f153d78bffc5651be0d199b30
MirunaTetcu <73702192+MirunaTetcu@users.noreply.github.com>
2022-01-10 02:54:26 +0200

    Delete Lab10-13 directory

--- Lab10-13/src/main/java/com/lab/entities/Element.java
+++
@@ -1,8 +0,0 @@
-package com.lab.entities;
-
-public interface Element {
-    void add(Element element) throws Exception;
-    void remove(Element element);
-    boolean find(Element element);
-}
-
--- Lab10-13/src/main/java/com/lab/repositories/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lab.repositories;
-
-import com.lab.entities.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
